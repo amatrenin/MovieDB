df4599eccea1c8299eb43c2b150949b33f071d81
amatrenin <104056823+amatrenin@users.noreply.github.com>
2022-07-24 14:17:22 +0300

    add recyclerView in the actors sections

--- app/src/main/java/com/example/mypetproject/view/MoviesDetailsActivity.kt
+++ app/src/main/java/com/example/mypetproject/view/MoviesDetailsActivity.kt
@@ -1,20 +1,28 @@
 package com.example.mypetproject.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mypetproject.R
 import com.example.mypetproject.data.Cast
 import com.example.mypetproject.data.MoviesDetails
+import com.example.mypetproject.view.adapters.CustomAdapter
+import com.example.mypetproject.view.adapters.CustomAdapterActors
 import com.example.mypetproject.viewmodel.MoviesViewModel
 import com.squareup.picasso.Picasso
 
 
-class MoviesDetailsActivity : AppCompatActivity() {
+class MoviesDetailsActivity : AppCompatActivity(), CustomAdapterActors.ItemClickListenerActors {
 
     private val mViewModel: MoviesViewModel = MoviesViewModel()
+
+    private lateinit var mMoviesActorsRecycler: RecyclerView
+    private lateinit var mMoviesActorsAdapter: CustomAdapterActors
 
     private lateinit var mTitle: TextView
     private lateinit var mTitleActors: TextView
@@ -47,12 +55,15 @@ class MoviesDetailsActivity : AppCompatActivity() {
         }
     }
 
-    private fun initObserversActors() {
+    private fun initObserversActors(actors: Cast) {
         mViewModel.apply {
             movieDetailsActors.observe(this@MoviesDetailsActivity) {
-                it.cast.forEach { cast ->
-                    setMovieInformationActors(cast)
-                }
+                mMoviesActorsAdapter = CustomAdapterActors(it, this@MoviesDetailsActivity)
+                mMoviesActorsRecycler.adapter = mMoviesActorsAdapter
+
+//                it.cast.forEach { cast ->
+//                    (cast)
+//                }
                 Log.d("testLogs", "observe ${it}")
             }
         }
@@ -70,22 +81,32 @@ class MoviesDetailsActivity : AppCompatActivity() {
             .into(mBanner)
     }
 
-    private fun setMovieInformationActors(movieDetailsActors: Cast?) {
-        mTitleActors.text = movieDetailsActors?.name
-
-        Picasso.get()
-            .load("https://image.tmdb.org/t/p/w500" + movieDetailsActors?.profile_path)
-            .into(mBannerActors)
-    }
+//    private fun setMovieInformationActors(movieDetailsActors: Cast?) {
+//        mTitleActors.text = movieDetailsActors?.name
+//
+//        Picasso.get()
+//            .load("https://image.tmdb.org/t/p/w500" + movieDetailsActors?.profile_path)
+//            .into(mBannerActors)
+    //   }
 
     private fun initViews() {
         mTitle = findViewById(R.id.movies_details_title)
-        mTitleActors = findViewById(R.id.movies_details_actor_name)
+        //      mTitleActors = findViewById(R.id.movies_details_actor_name)
         mReliaseDate = findViewById(R.id.movies_details_date)
         mScore = findViewById(R.id.movies_details_score)
         mOverview = findViewById(R.id.movies_details_body_overview)
         mBanner = findViewById(R.id.movies_details_image_banner)
-        mBannerActors = findViewById(R.id.movies_details_actors_banner)
+        mBannerActors = findViewById(R.id.imageViewActors)
 
+
+        mMoviesActorsRecycler = findViewById(R.id.recyclerview)
+        mMoviesActorsRecycler.layoutManager = GridLayoutManager(this, 2)
+
+    }
+
+    override fun onItemClickActors(id: Int) {
+        val intent = Intent(this, MoviesActivityActors::class.java)
+        intent.putExtra("id", id)
+        startActivity(intent)
     }
 }
