ea0de6ece5a8b140c0ad1d3741c51885965384ec
amatrenin <104056823+amatrenin@users.noreply.github.com>
2022-08-06 21:21:32 +0300

    add Dagger-Hilt

--- app/src/main/java/com/example/mypetproject/di/DataModule.kt
+++ app/src/main/java/com/example/mypetproject/di/DataModule.kt
@@ -1,14 +1,17 @@
-//package com.example.mypetproject.di
-//
-//import com.example.mypetproject.ApiInterface
-//import com.example.mypetproject.model.repository.MoviesDBRepository
-//import com.example.mypetproject.model.repository.MoviesDBRepositoryImpl
-//import dagger.Module
-//
-//
-//@Module
-//class DataModule {
-//    fun provideMovieRepository(apiInterface: ApiInterface) : MoviesDBRepository {
-//       return MoviesDBRepositoryImpl(apiInterface = apiInterface)
-//    }
-//}
+package com.example.mypetproject.di
+
+import com.example.mypetproject.ApiInterface
+import com.example.mypetproject.model.repository.MoviesDBRepository
+import com.example.mypetproject.model.repository.MoviesDBRepositoryImpl
+import dagger.Module
+
+
+@Module
+class DataModule {
+
+    fun provideMovieApiInterface()
+
+    fun provideMovieRepository(apiInterface: ApiInterface) : MoviesDBRepository {
+       return MoviesDBRepositoryImpl(apiInterface = apiInterface)
+    }
+}
